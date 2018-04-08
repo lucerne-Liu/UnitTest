4494e07a51ca1a483bfb9a175bda87670293cb4d
Lucerne <lucerne_liu@foxmail.com>
2018-04-08 23:56:42 +0800

    add AnswerTest "should_validate_throw_OutOfRangeAnswerException_when_answer_contains_same_numbers"

--- src/test/java/tw/core/AnswerTest.java
+++ src/test/java/tw/core/AnswerTest.java
@@ -1,7 +1,10 @@
 package tw.core;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import tw.core.exception.OutOfRangeAnswerException;
 
 import java.util.Arrays;
 import static org.junit.Assert.*;
@@ -9,6 +12,9 @@ import static org.junit.Assert.*;
  * 在AnswerTest文件中完成Answer中对应的单元测试
  */
 public class AnswerTest {
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
     private Answer answer;
 
     @Before
@@ -21,5 +27,13 @@ public class AnswerTest {
         Answer result = new Answer();
         result.setNumList(Arrays.asList("1","2","3","4"));
         assertEquals(result.toString(), answer.createAnswer("1 2 3 4").toString());
+    }
+
+    @Test
+    public void should_validate_throw_OutOfRangeAnswerException_when_answer_contains_same_numbers() throws OutOfRangeAnswerException {
+        answer.setNumList(Arrays.asList("1", "2", "3","1"));
+        exception.expect(OutOfRangeAnswerException.class);
+        exception.expectMessage("Answer format is incorrect");
+        answer.validate();
     }
 }
