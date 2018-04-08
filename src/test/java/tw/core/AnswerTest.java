6ae0b7feaf50c0b20781d79e707471f7af9fb0b3
Lucerne <lucerne_liu@foxmail.com>
2018-04-08 23:44:44 +0800

    add AnswerTest "should_createAnswer_return_answer_when_input_string"

--- src/test/java/tw/core/AnswerTest.java
+++ src/test/java/tw/core/AnswerTest.java
@@ -1,8 +1,25 @@
 package tw.core;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+import static org.junit.Assert.*;
 /**
  * 在AnswerTest文件中完成Answer中对应的单元测试
  */
 public class AnswerTest {
+    private Answer answer;
 
+    @Before
+    public final void before() {
+        answer = new Answer();
+    }
+
+    @Test
+    public void should_createAnswer_return_answer_when_input_string() {
+        Answer result = new Answer();
+        result.setNumList(Arrays.asList("1","2","3","4"));
+        assertEquals(result.toString(), answer.createAnswer("1 2 3 4").toString());
+    }
 }
