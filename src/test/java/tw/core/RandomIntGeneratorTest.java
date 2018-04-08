910f34313cd3ed2e61a49aa453781b769ac04f9d
Lucerne <lucerne_liu@foxmail.com>
2018-04-08 20:52:47 +0800

    add test "should_throw_IllegalArgumentException_when_digitmax_less_than_numbersOfNeed"

--- src/test/java/tw/core/RandomIntGeneratorTest.java
+++ src/test/java/tw/core/RandomIntGeneratorTest.java
@@ -1,9 +1,29 @@
 package tw.core;
 
 
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import tw.core.generator.RandomIntGenerator;
+
 /**
  * 在RandomIntGeneratorTest文件中完成RandomIntGenerator中对应的单元测试
  */
 public class RandomIntGeneratorTest {
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+    private RandomIntGenerator randomIntGenerator;
 
+    @Before
+    public final void before() {
+        randomIntGenerator = new RandomIntGenerator();
+    }
+    
+    @Test
+    public void should_throw_IllegalArgumentException_when_digitmax_less_than_numbersOfNeed(){
+        exception.expect(IllegalArgumentException.class);
+        exception.expectMessage("Can't ask for more numbers than are available");
+        randomIntGenerator.generateNums(2, 4);
+    }
 }
