ce438951089ca9a4fada78a987195080c575bbe3
Lucerne <lucerne_liu@foxmail.com>
2018-04-09 00:47:43 +0800

    add AnswerGeneratorTest "should_return_answer_the_same_as_generateNums"

--- src/test/java/tw/core/generator/AnswerGeneratorTest.java
+++ src/test/java/tw/core/generator/AnswerGeneratorTest.java
@@ -4,10 +4,14 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+import tw.core.Answer;
 import tw.core.exception.OutOfRangeAnswerException;
+
+import java.util.Arrays;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static org.junit.Assert.*;
 
 /**
  * 在AnswerGeneratorTest文件中完成AnswerGenerator中对应的单元测试
@@ -39,6 +43,14 @@ public class AnswerGeneratorTest {
         exception.expect(OutOfRangeAnswerException.class);
         exception.expectMessage("Answer format is incorrect");
         answerGenerator.generate();
+    }
+
+    @Test
+    public void should_return_answer_the_same_as_generateNums() throws OutOfRangeAnswerException {
+        when(randomIntGenerator.generateNums(9, 4)).thenReturn("1 2 3 4");
+        Answer answer = new Answer();
+        answer.setNumList(Arrays.asList("1","2","3","4"));
+        assertEquals(answer.toString(), answerGenerator.generate().toString());
     }
 }
 
